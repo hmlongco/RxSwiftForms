96c61666b3693e0f0a814a54dedeeba7c29b226d
Michael Long <mlong@clientresourcesinc.com>
2018-03-25 08:50:08 -0500

    Initial Commit

---
+++ RxSwiftForms/RxSwiftForms.h
@@ -0,0 +1,19 @@
+//
+//  RxSwiftForms.h
+//  RxSwiftForms
+//
+//  Created by Michael Long on 3/25/18.
+//  Copyright © 2018 Michael Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxSwiftForms.
+FOUNDATION_EXPORT double RxSwiftFormsVersionNumber;
+
+//! Project version string for RxSwiftForms.
+FOUNDATION_EXPORT const unsigned char RxSwiftFormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwiftForms/PublicHeader.h>
+
+
---
+++ RxSwiftFormsTests/RxSwiftFormsTests.swift
@@ -0,0 +1,36 @@
+//
+//  RxSwiftFormsTests.swift
+//  RxSwiftFormsTests
+//
+//  Created by Michael Long on 3/25/18.
+//  Copyright © 2018 Michael Long. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwiftForms
+
+class RxSwiftFormsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
