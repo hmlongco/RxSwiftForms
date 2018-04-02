7b02c7f0a99757813ace17feb272146de66adc1b
Michael Long <mlong@clientresourcesinc.com>
2018-04-02 13:38:06 -0500

    Update jazzy shell script and api documentation files

---
+++ jazzy.sh
@@ -0,0 +1,7 @@
+jazzy \
+  --clean \
+  --author "Michael Long" \
+  --github_url https://github.com/hmlongco/RxSwiftForms \
+  --module RxSwiftForms \
+  --xcodebuild-arguments -workspace,RxSwiftForms.xcworkspace,-scheme,RxSwiftForms \
+  --output ./Documentation/API
