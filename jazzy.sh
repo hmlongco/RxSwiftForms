be647bfeb4e1ac583f74d453d7042319e06cdfef
Michael Long <mlong@clientresourcesinc.com>
2018-04-02 13:47:00 -0500

    Document RxSwiftForms only

--- jazzy.sh
+++ jazzy.sh
@@ -4,4 +4,5 @@ jazzy \
   --github_url https://github.com/hmlongco/RxSwiftForms \
   --module RxSwiftForms \
   --xcodebuild-arguments -workspace,RxSwiftForms.xcworkspace,-scheme,RxSwiftForms \
+  --exclude /*/MetaTextField* \
   --output ./Documentation/API
