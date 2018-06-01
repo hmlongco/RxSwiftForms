dd5f6815594fa7646d385a07e780181119ec03f8
Michael Long <mlong@clientresourcesinc.com>
2018-05-31 23:34:54 -0500

    Add FxValueFactory support

--- Sources/RxSwiftForms/Values/FxValues.swift
+++ Sources/RxSwiftForms/Values/FxValues.swift
@@ -8,9 +8,19 @@
 
 import Foundation
 
+public protocol FxValueFactory {
+    func make<T>(_ value:T) -> FxValue
+}
+
 open class FxValues {
 
     public static var instance = FxValues()
+
+    public static var types = [Int:FxValueFactory]()
+
+    public static func register<T>(_ type: T, factory: FxValueFactory) {
+        FxValues.types[ObjectIdentifier(T.self).hashValue] = factory
+    }
 
     public static func make<T>(_ value: T?) -> FxValue {
         return instance.make(value)
@@ -25,6 +35,9 @@ open class FxValues {
         }
         if let value = value as? Bool {
             return FxValueBool(value)
+        }
+        if let factory = FxValues.types[ObjectIdentifier(T.self).hashValue] {
+            return factory.make(value)
         }
         return FxValueGeneric(value)
     }
